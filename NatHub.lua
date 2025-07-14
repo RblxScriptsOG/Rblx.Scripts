069c2893d467994686649011661f6204b4c2115f
RblxScriptsOG <faithsmiley.robloxscripts@gmail.com>
2025-07-14 10:22:13 +0500

    Update NatHub.lua

--- NatHub.lua
+++ NatHub.lua
@@ -3,7 +3,7 @@
 --Working On Grow a Garden's Update: Pet Mutation
 
 --NatHub:
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/NatHub.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/NatHub.lua"))()
 
 --BackUp:
 loadstring(game:HttpGet("https://raw.githubusercontent.com/RblxScriptsOG/Rblx.Scripts/refs/heads/main/Script"))()
