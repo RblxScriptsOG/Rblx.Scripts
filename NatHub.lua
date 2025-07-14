0bcbe886e184d32df64563415f6320db86549e31
RblxScriptsOG <faithsmiley.robloxscripts@gmail.com>
2025-07-14 12:49:50 +0500

    Update and rename NatHub.lua to da

--- NatHub.lua
+++
@@ -1,9 +0,0 @@
--- Status: Working🟢
---Last Updated: Sunday, 13 July 2025
---Working On Grow a Garden's Update: Pet Mutation
-
---NatHub:
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/NatHub.lua"))()
-
---BackUp:
-loadstring(game:HttpGet("https://raw.githubusercontent.com/RblxScriptsOG/Rblx.Scripts/refs/heads/main/Script"))()
