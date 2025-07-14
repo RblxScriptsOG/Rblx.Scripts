340eeea63e7a1ce01a677fac27e6d04bac30d0c6
RblxScriptsOG <faithsmiley.robloxscripts@gmail.com>
2025-07-14 12:49:15 +0500

    Update and rename No-Lag.lua to Famous Scripts

--- No-Lag.lua
+++
@@ -1,9 +0,0 @@
--- Status: Working🟢
---Last Updated: Sunday, 13 July 2025
---Working On Grow a Garden's Update: Pet Mutation
-
---NO-LAG:
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/Main.lua"))()
-
---BackUp:
-loadstring(game:HttpGet("https://raw.githubusercontent.com/RblxScriptsOG/Rblx.Scripts/refs/heads/main/Script"))()
